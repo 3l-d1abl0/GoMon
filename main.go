d6b02878e1e46365d7b27596e3a7547083cc03fb
Sameer <sameer.barha12@gmail.com>
2024-01-18 20:35:48 +0530

    added feat to gracefully handle Server Shutdown

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"GoMon/api"
+	"context"
 	"fmt"
-	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -23,7 +27,56 @@ func init() {
 }
 func main() {
 
+	//Get the router
 	var app *gin.Engine = api.Setup()
-	log.Fatal(app.Run(":" + os.Getenv("SERVERPORT")))
-	fmt.Println("Serving.... ")
+
+	server := &http.Server{
+		Addr:    ":" + os.Getenv("SERVERPORT"),
+		Handler: app,
+	}
+
+	//Channel to capture interrupt
+	quit := make(chan os.Signal)
+
+	//Notify the quit channel
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	//another Goroutine to handle interrupt and shutdown gracefully
+	go func() {
+
+		//Starting Server
+		fmt.Printf("Starting Server on : %s\n", os.Getenv("SERVERPORT"))
+		if err := server.ListenAndServe(); err != nil {
+			if err == http.ErrServerClosed {
+				fmt.Printf("Server closed under request : %s\n", err)
+				//panic(fmt.Sprintf("Server closed under request : %s\n", err))
+			} else {
+				fmt.Printf("Server closed unexpect : %s\n", err)
+				//panic(fmt.Sprintf("Server closed unexpect : %s\n", err))
+			}
+		}
+
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server
+	<-quit
+	fmt.Println("Trying to Shutdown Server !")
+
+	//Context with a timeout of 5 seconds.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	//Server shutdown
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Printf("Error while Server Shutdown: %s\n", err)
+	}
+
+	// catching ctx.Done(). timeout of 5 seconds.
+	select {
+	case <-ctx.Done():
+		fmt.Println("Timeout :5 sec")
+	}
+
+	fmt.Println("Exiting Server !")
+
 }
