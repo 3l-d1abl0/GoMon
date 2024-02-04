2c7caf82b3a148b9bf000c9a6dec4fb801586f63
Sameer <sameer.barha12@gmail.com>
2024-02-04 06:04:25 +0530

    added structure for CPUInfo

--- commonData/common.go
+++ commonData/common.go
@@ -16,3 +16,23 @@ type NetworkInterface struct {
 type NetworkInfo struct {
 	Interfaces []NetworkInterface `json:"networkInfo"`
 }
+
+type CPUInfo struct {
+	CPU        int      `json:"cpu"`
+	VendorID   string   `json:"vendorId"`
+	Family     string   `json:"family"`
+	Model      string   `json:"model"`
+	Stepping   int      `json:"stepping"`
+	PhysicalID string   `json:"physicalId"`
+	CoreID     string   `json:"coreId"`
+	Cores      int      `json:"cores"`
+	ModelName  string   `json:"modelName"`
+	MHz        int      `json:"mhz"`
+	CacheSize  int      `json:"cacheSize"`
+	Flags      []string `json:"flags"`
+	Microcode  string   `json:"microcode"`
+}
+
+type CPUInfoList struct {
+	CPUInfoList []CPUInfo `json:"cpuInfo"`
+}
